664956f1a09ba99a50b5612f3d808b410c356d6b
EDU/a.korokhov <a.korokhov@innopolis.university>
2022-05-26 08:46:03 +0300

    add slippage section

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '../styles/fontawesome.css'
+import '../styles/inter.css'
 
 function MyApp({ Component, pageProps }) {
-
   return <Component {...pageProps} />
 }
 

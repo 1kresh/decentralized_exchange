8c058d09789288cac1e2c5db0d787d07123ca812
EDU/a.korokhov <a.korokhov@innopolis.university>
2022-05-27 07:26:59 +0300

    swap to sass

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import IMask from 'imask'
 import {useEffect} from "react";
 
@@ -81,7 +81,6 @@ export default function Swap() {
       mask: Number,
       scale: 0, 
       signed: false,
-      radix: '.',
       min: 1,
       max: 4320
     }
@@ -174,7 +173,7 @@ export default function Swap() {
     <svg class=${styles.slip_svg} xmlns="http://www.w3.org/2000/svg" version="1.1" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
     <g><path d="M 362.5,-0.5 C 369.5,-0.5 376.5,-0.5 383.5,-0.5C 415.109,9.01599 429.609,30.3493 427,63.5C 421.806,88.6937 406.64,103.86 381.5,109C 353.542,112.264 333.375,101.431 321,76.5C 311.523,48.6099 318.356,25.7766 341.5,8C 348.187,4.05686 355.187,1.22353 362.5,-0.5 Z M 342.5,511.5 C 336.5,511.5 330.5,511.5 324.5,511.5C 312.396,508.56 305.23,500.893 303,488.5C 303.484,445.172 303.484,401.839 303,358.5C 283.69,345.871 263.856,334.037 243.5,323C 235.123,315.621 226.456,308.621 217.5,302C 207.272,291.764 200.438,279.597 197,265.5C 195.364,253.348 197.697,242.014 204,231.5C 224.292,199.084 244.459,166.584 264.5,134C 239.5,133.333 214.5,133.333 189.5,134C 186.078,137.003 183.245,140.503 181,144.5C 174.396,156.098 167.396,167.431 160,178.5C 146.117,193.107 132.117,193.107 118,178.5C 112.917,168.159 113.583,158.159 120,148.5C 131.584,130.334 142.917,112.001 154,93.5C 157.649,88.7585 162.482,86.2585 168.5,86C 179,85.8333 189.5,85.6667 200,85.5C 230.209,85.6734 260.376,86.5067 290.5,88C 294.729,89.1146 298.729,90.7813 302.5,93C 323.672,106.752 344.672,120.752 365.5,135C 373.386,140.886 377.886,148.72 379,158.5C 379.5,180.497 379.667,202.497 379.5,224.5C 404.538,224.058 429.538,224.558 454.5,226C 467.502,230.484 473.002,239.651 471,253.5C 468.833,263.667 462.667,269.833 452.5,272C 416.167,272.667 379.833,272.667 343.5,272C 337.553,270.387 334.053,266.553 333,260.5C 332.667,243.5 332.333,226.5 332,209.5C 317.363,232.302 302.53,254.968 287.5,277.5C 309.998,291.917 332.665,306.084 355.5,320C 360.285,324.069 363.119,329.236 364,335.5C 364.667,386.167 364.667,436.833 364,487.5C 362.101,500.342 354.934,508.342 342.5,511.5 Z M 419.5,192.5 C 430.838,192.333 442.172,192.5 453.5,193C 456.705,194.415 458.372,196.915 458.5,200.5C 456.837,205.325 454.504,209.825 451.5,214C 441.5,214.667 431.5,214.667 421.5,214C 418.067,210.068 415.734,205.568 414.5,200.5C 414.672,196.807 416.339,194.141 419.5,192.5 Z M 178.5,276.5 C 187.462,297.643 201.462,314.81 220.5,328C 221.804,328.804 222.471,329.971 222.5,331.5C 212.279,356.943 201.779,382.276 191,407.5C 183.931,417.891 174.764,420.058 163.5,414C 125.948,398.057 88.2813,382.391 50.5,367C 39.1743,363.017 33.5076,355.017 33.5,343C 33.6918,320.815 44.6918,309.148 66.5,308C 70.2765,308.592 73.9431,309.592 77.5,311C 101.506,321.224 125.673,331.058 150,340.5C 159.74,319.347 169.24,298.013 178.5,276.5 Z M 420.5,280.5 C 432.886,280.106 445.22,280.606 457.5,282C 462.208,283.378 465.708,286.212 468,290.5C 473.076,307.872 476.076,325.538 477,343.5C 473.708,356.794 465.541,365.627 452.5,370C 441.833,370.667 431.167,370.667 420.5,370C 401.82,363.491 393.653,350.324 396,330.5C 398.667,317.833 401.333,305.167 404,292.5C 407.216,285.16 412.716,281.16 420.5,280.5 Z"/></g>
     </svg>
-    <span class=${styles.tooltiptext}>Your transaction may be frontrun.</span>
+    <span class=${styles.tooltiptext_down}>Your transaction may be frontrun.</span>
     `
   }
 
@@ -182,7 +181,7 @@ export default function Swap() {
     elem.innerHTML = `<svg class=${styles.slip_svg} id="svg" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0, 0, 400,400">
     <g id="svgg"><path id="path0" d="M94.646 1.553 C 85.342 4.917,78.447 11.163,73.996 20.258 L 71.484 25.391 71.094 126.172 L 70.703 226.953 68.685 231.250 C 67.575 233.613,63.739 239.414,60.159 244.141 C 56.579 248.867,51.659 255.371,49.225 258.594 C 6.446 315.231,20.108 383.647,77.083 398.099 L 84.575 400.000 179.592 399.995 C 271.356 399.990,274.686 399.939,276.843 398.517 C 282.431 394.833,282.020 401.550,282.026 313.761 L 282.031 234.943 289.373 235.327 C 305.527 236.172,304.987 234.487,305.469 285.547 L 305.859 326.953 308.371 332.085 C 321.648 359.217,359.530 359.415,372.719 332.422 L 375.391 326.953 375.601 235.547 L 375.812 144.141 373.802 137.127 C 367.980 116.802,351.941 101.163,331.711 96.083 C 326.284 94.720,322.886 94.531,303.812 94.531 L 282.093 94.531 281.867 59.961 L 281.641 25.391 278.969 19.922 C 275.472 12.766,269.265 6.559,262.109 3.063 L 256.641 0.391 177.734 0.216 C 101.432 0.047,98.690 0.091,94.646 1.553 M252.344 25.532 C 258.610 29.437,258.176 24.213,258.420 98.633 L 258.636 164.844 176.563 164.844 L 94.489 164.844 94.705 98.633 L 94.922 32.422 96.635 29.688 C 100.650 23.280,96.835 23.560,178.125 23.703 L 249.609 23.828 252.344 25.532 M123.157 49.139 C 117.762 52.696,117.979 50.791,117.979 94.531 C 117.979 138.271,117.762 136.366,123.157 139.924 C 126.629 142.213,226.496 142.213,229.968 139.924 C 235.363 136.366,235.146 138.271,235.146 94.531 C 235.146 50.791,235.363 52.696,229.968 49.139 C 226.496 46.850,126.629 46.850,123.157 49.139 M211.719 94.531 L 211.719 117.969 200.000 117.969 L 188.281 117.969 188.276 109.961 C 188.269 98.511,185.254 94.542,176.563 94.542 C 167.871 94.542,164.856 98.511,164.849 109.961 L 164.844 117.969 153.125 117.969 L 141.406 117.969 141.406 94.531 L 141.406 71.094 176.563 71.094 L 211.719 71.094 211.719 94.531 M305.620 140.430 L 305.859 162.891 308.371 168.023 C 315.237 182.053,325.918 188.281,343.112 188.281 L 352.386 188.281 352.170 254.102 C 351.958 318.336,351.915 319.984,350.391 322.515 C 345.876 330.007,335.374 330.007,330.859 322.515 C 329.363 320.032,329.280 318.163,328.906 278.516 L 328.516 237.109 325.844 231.641 C 319.318 218.284,309.296 212.288,292.773 211.852 L 282.031 211.569 282.031 164.769 L 282.031 117.969 293.706 117.969 L 305.381 117.969 305.620 140.430 M342.356 129.623 C 349.363 137.276,352.252 146.105,352.320 160.067 L 352.344 165.057 345.042 164.675 C 330.512 163.915,329.387 162.040,329.037 138.006 L 328.777 120.152 333.570 122.707 C 336.299 124.161,340.083 127.140,342.356 129.623 M258.594 282.422 L 258.594 376.563 203.156 376.563 L 147.719 376.563 151.406 371.680 C 169.663 347.503,169.586 311.835,151.226 288.672 C 144.645 280.369,142.384 276.155,141.797 271.098 C 140.883 263.221,136.917 259.385,129.688 259.385 C 119.877 259.385,118.459 262.077,117.963 281.641 C 117.613 295.426,117.383 297.571,116.010 299.859 C 111.513 307.350,100.999 307.350,96.484 299.859 C 94.968 297.341,94.915 295.674,94.701 242.773 L 94.481 188.281 176.537 188.281 L 258.594 188.281 258.594 282.422 M74.156 309.766 C 86.146 334.303,121.523 336.757,136.109 314.063 L 137.364 312.109 138.752 315.234 C 140.771 319.780,141.199 334.050,139.514 340.628 C 132.811 366.791,105.144 382.208,80.276 373.637 C 43.549 360.978,37.473 317.212,66.727 276.046 L 70.703 270.452 71.094 287.374 L 71.484 304.297 74.156 309.766 "></path></g>
     </svg>
-    <span class=${styles.tooltiptext}>Your transaction may fail.</span>`
+    <span class=${styles.tooltiptext_down}>Your transaction may fail.</span>`
   }
 
   const focusInput = (event) => {
@@ -339,8 +338,8 @@ export default function Swap() {
     [btn0.innerHTML, btn1.innerHTML] = [btn1.innerHTML, btn0.innerHTML];
     [balance0.innerHTML, balance1.innerHTML] = [balance1.innerHTML, balance0.innerHTML];
     [token0, token1] = [token1, token0];
-    numberMask0.updateValue();
-    numberMask1.updateValue();
+    token0_input_mask.updateValue();
+    token1_input_mask.updateValue();
   }
 
   const setAutoSlipValue = (event) => {
@@ -476,51 +475,72 @@ export default function Swap() {
         </div>
           
         <div id={styles.settings_div}>
-          <div className={styles.settings_title}>Transaction Settings</div>
-          <div className={styles.tx_settings_div}>
-            <div className={styles.info_row}>
-              <div>
-                Slippage tolerance
+          <div className={styles.tx_settings_div_main}>
+            <div className={`${styles.tx_settings_title} ${styles.settings_title}`}>Transaction Settings</div>
+            <div className={styles.tx_settings_div}>
+              <div className={styles.tx_settings_div_micro}>
+                <div className={styles.info_row}>
+                  <div>
+                    Slippage tolerance
+                  </div>
+                  <div className={`${styles.question_icon} ${styles.tooltip}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#6e6e6e" className="bi bi-question-lg" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z"/>
+                    </svg>
+                    <span class={`${styles.tooltiptext_down} ${styles.tooltiptext_down_tip}`}>Your transaction will revert if the price changes unfavorably by more than this percentage.</span>
+                  </div>
+                </div>
+                <div className={styles.slip_input_div_main}>
+                  <div id={styles.auto_btn} className={styles.activated_auto_btn} onClick={setAutoSlipValue}>
+                    Auto
+                  </div>
+                  <div className={styles.slip_input_div}>
+                    <span id="slip_warnings" className={`${styles.slip_svg_span} ${styles.tooltip}`}>
+                    </span>
+                    <input id="slip_input" className={`${styles.slip_input_field} ${styles.no_outline}`} inputMode="decimal" autoComplete="off" autoCorrect="off" autofill="off" type="text" pattern="^[0-9]*[.,]?[0-9]*$" placeholder="0.10" spellCheck="false"></input>
+                    %
+                  </div>
+                </div>
               </div>
-              <div className={`${styles.question_icon} ${styles.tooltip}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-question-lg" viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z"/>
-                </svg>
-                <span class={styles.tooltiptext}>Your transaction will revert if the price changes unfavorably by more than this percentage.</span>
+              <div className={styles.tx_settings_div_micro}>
+                <div className={styles.info_row}>
+                  <div>
+                    Transaction deadline
+                  </div>
+                  <div className={`${styles.question_icon} ${styles.tooltip}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#6e6e6e" className="bi bi-question-lg" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z"/>
+                    </svg>
+                    <span class={`${styles.tooltiptext_down} ${styles.tooltiptext_down_tip}`}>Your transaction will revert if it is pending for more than this period of time.</span>
+                  </div>
+                </div>
+                <div className={styles.dl_input_div_main}>
+                  <div className={styles.dl_input_div}>
+                  <input id="dl_input" className={`${styles.dl_input_field} ${styles.no_outline}`} inputMode="numeric" autoComplete="off" autoCorrect="off" autofill="off" type="text" pattern="^[0-9]*[.,]?[0-9]*$" placeholder="30" spellCheck="false"></input>
+                  </div>
+                  <div class={styles.minutes_div}>minutes</div>
+                </div>
               </div>
             </div>
-            <div className={styles.slip_input_div_main}>
-              <div id={styles.auto_btn} className={styles.activated_auto_btn} onClick={setAutoSlipValue}>
-                Auto
+          </div>
+          <div className={styles.intrfc_settings_div_main}>
+            <div className={`${styles.intrfc_settings_title} ${styles.settings_title}`}>Interface Settings</div>
+            <div>
+              <div className={styles.info_row}>
+                <div>
+                  Expert Mode
+                </div>
+                <div className={`${styles.question_icon} ${styles.tooltip}`}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#6e6e6e" className="bi bi-question-lg" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z"/>
+                  </svg>
+                  <span class={`${styles.tooltiptext_up} ${styles.tooltiptext_up_tip}`}>Allow high price impact trades and skip the confirm screen. Use at your own risk.</span>
+                </div>
               </div>
-              <div className={styles.slip_input_div}>
-                <span id="slip_warnings" className={`${styles.slip_svg_span} ${styles.tooltip}`}>
-                </span>
-                <input id="slip_input" className={`${styles.slip_input_field} ${styles.no_outline}`} inputMode="decimal" autoComplete="off" autoCorrect="off" autofill="off" type="text" pattern="^[0-9]*[.,]?[0-9]*$" placeholder="0.10" spellCheck="false"></input>
-                %
-              </div>
-            </div>
-            <div className={styles.info_row}>
-              <div>
-                Transaction deadline
-              </div>
-              <div className={`${styles.question_icon} ${styles.tooltip}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-question-lg" viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14Z"/>
-                </svg>
-                <span class={styles.tooltiptext}>Your transaction will revert if it is pending for more than this period of time.</span>
-              </div>
-            </div>
-            <div className={styles.dl_input_div_main}>
-              <div className={styles.dl_input_div}>
-              <input id="dl_input" className={`${styles.dl_input_field} ${styles.no_outline}`} inputMode="numeric" autoComplete="off" autoCorrect="off" autofill="off" type="text" pattern="^[0-9]*[.,]?[0-9]*$" placeholder="30" spellCheck="false"></input>
-              </div>
-              <div class={styles.minutes_div}>minutes</div>
             </div>
           </div>
         </div>
       </div>
-
     </main>
   </div>
   )
